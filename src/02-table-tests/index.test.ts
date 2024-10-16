320181524746b1361cb75de00d6ece2f348c88c7
Aleh Kuis <pestleralex@gmail.com>
2024-10-16 16:59:29 +0300

    feat: 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,35 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
+  { a: 2, b: 2, action: Action.Add, expected: 4 },
+  { a: 3, b: 2, action: Action.Add, expected: 5 },
+  { a: 3, b: 3, action: Action.Add, expected: 6 },
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
-  });
-  // Consider to use Jest table tests API to test all cases above
+  { a: 3, b: 3, action: Action.Divide, expected: 1 },
+  { a: 2, b: 2, action: Action.Divide, expected: 1 },
+  { a: -5, b: -5, action: Action.Divide, expected: 1 },
+
+  { a: 3, b: 3, action: Action.Multiply, expected: 9 },
+  { a: 1, b: 1, action: Action.Multiply, expected: 1 },
+  { a: 3, b: -3, action: Action.Multiply, expected: -9 },
+
+  { a: 3, b: 3, action: Action.Exponentiate, expected: 27 },
+  { a: 1, b: 1, action: Action.Exponentiate, expected: 1 },
+  { a: 10, b: 10, action: Action.Exponentiate, expected: 10000000000 },
+
+  { a: 3, b: 3, action: 'invalid', expected: null },
+
+  { a: [], b: 3, action: Action.Add, expected: null },
+  { a: {}, b: 3, action: Action.Add, expected: null },
+];
+
+describe('simpleCalculator', (): void => {
+  test.each(testCases)(
+    'should perform $action on $a and $b and return $expected',
+    ({ a, b, action, expected }): void => {
+      expect(simpleCalculator({ a, b, action })).toBe(expected);
+    },
+  );
 });
